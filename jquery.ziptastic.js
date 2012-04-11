17b324828fb476621012e21c8d5137801d2c0dfd
Randy Merrill <Zoramite@gmail.com>
2012-04-10 18:52:58 -0700

    Rewriting the jQuery plugin to be more in line with normal jQuery methodology.

---
+++ jquery.ziptastic.js
@@ -0,0 +1,39 @@
+(function( $ ) {
+	var requests = {};
+	var zipValid = {
+		us: /[0-9]{5}(-[0-9]{4})/
+	};
+
+	$.ziptastic = function(zip, callback){
+		// Only make unique requests
+		if(!requests[zip]) {
+			requests[zip] = $.getJSON('http://zip.elevenbasetwo.com?zip=' + zip);
+		}
+
+		// Bind to the finished request
+		requests[zip].done(function(data) {
+			callback(data.country, data.state, data.city, zip);
+		});
+
+		// Allow for binding to the deferred object
+		return requests[zip];
+	};
+
+	$.fn.ziptastic = function( options ) {
+		return this.each(function() {
+			var ele = $(this);
+
+			ele.on('input change', function() {
+				var zip = ele.val();
+
+				// TODO Non-US zip codes?
+				if(zipValid.us.test(zip)) {
+					$.ziptastic(zip, function(country, state, city) {
+						// Trigger the updated information
+						ele.trigger('zipChange', [country, state, city, zip]);
+					})
+				}
+			});
+		});
+	};
+})( jQuery );

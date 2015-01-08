52e118ba09474422e35950f8b58b1da251e02e29
Thomas Schultz <daspecster@gmail.com>
2015-01-07 21:53:09 -0500

    Support version 3 of GetZiptastic.com API. closes #23
    - Switch to v3 payload lists
    - Add Key headers
    - Make API key message more noticeable.

--- jquery.ziptastic.js
+++ jquery.ziptastic.js
@@ -4,19 +4,18 @@
 		us: /[0-9]{5}(-[0-9]{4})?/
 	};
 
-	$.ziptastic = function(country, zip, callback){
-		// If only zip and callback are given default to US
-		if (arguments.length == 2 && typeof arguments[1] == 'function') {
-			callback = arguments[1];
-			zip = arguments[0];
-			country = 'US';
-		}
+	$.ziptastic = function(country, zip, key, callback){
+		country = country || 'US';
+        if (!key) {
+            alert("Please register for an API key at GetZiptastic.com.");
+            return;
+        }
 
 		country = country.toUpperCase();
-		// Only make unique requests
 		if(!requests[country]) {
 			requests[country] = {};
 		}
+
 		if(!requests[country][zip]) {
 			var protocol = '';
 			if (location.protocol == 'file:') {
@@ -24,16 +23,31 @@
 			} else {
 				protocol = location.protocol;
 			}
-			requests[country][zip] = $.getJSON(protocol + '//zip.getziptastic.com/v2/' + country + '/' + zip);
+
+			var referrer = document.location.origin + document.location.pathname;
+			requests[country][zip] = $.ajax({
+				url: protocol + '//zip.getziptastic.com/v3/' + country + '/' + zip,
+				headers: {'x-referrer': referrer, 'x-key': key},
+				contentType: "application/json",
+				type: 'GET',
+				dataType: 'json',
+				success: function(data) { requests[country][zip] = data[0]; },
+				error: function(e) { console.log('There was an error. ' + e.message ); }
+			});
+
+			// Bind to the finished request
+			requests[country][zip].done(function(data) {
+
+				if (typeof callback == 'function') {
+					var data_temp = data
+					var key = Object.keys(data_temp)[0];
+
+					requests[country][zip] = data_temp[key];
+					callback(data_temp[key].country, data_temp[key].state,
+                             data_temp[key].state_short, data_temp[key].city, zip);
+				}
+			});
 		}
-
-		// Bind to the finished request
-		requests[country][zip].done(function(data) {
-			if (typeof callback == 'function') {
-				callback(data.country, data.state, data.state_short, data.city, zip);
-			}
-		});
-
 		// Allow for binding to the deferred object
 		return requests[country][zip];
 	};
@@ -41,13 +55,10 @@
 	$.fn.ziptastic = function( options ) {
 		return this.each(function() {
 			var ele = $(this);
-
 			ele.on('keyup change', function() {
 				var zip = ele.val();
-
-				// TODO Non-US zip codes?
 				if(zipValid.us.test(zip)) {
-					$.ziptastic(zip, function(country, state, state_short, city) {
+					$.ziptastic(options.country, zip, options.key, function(country, state, state_short, city) {
 						// Trigger the updated information
 						ele.trigger('zipChange', [country, state, state_short, city, zip]);
 					});
